720dc603b946820dd9c01855024e3e082d54a887
ter <ter@localhost>
2012-04-10 00:06:47 -0400

    Starting 'pc-log4j' branch

--- RedShift/src/core/CoreStateManager.java
+++ RedShift/src/core/CoreStateManager.java
@@ -1,5 +1,6 @@
 package core;
 
+import org.apache.log4j.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
@@ -9,40 +10,44 @@ import ents.EntityFactory;
 
 public class CoreStateManager extends StateBasedGame {
 
-	//vars
+	private static Logger logger = Logger.getLogger(CoreStateManager.class);
+
+	// vars
 	public static int LOADERSTATE = 1;
 	public static int GAMEPLAYSTATE = 2;
 	public static int CLIENTLOADSTATE = 3;
 	public static int CLIENTPLAYSTATE = 4;
-	
+
 	public GameDatabase gDB;
 	public PlayerClient player;
 	public EntityFactory entFac;
-	
-	//const
+
+	// const
 	public CoreStateManager() {
 		super("Project RedShift");
 		gDB = new GameDatabase();
 		player = new PlayerClient(1);
 		entFac = new EntityFactory(gDB);
-//		this.addState(new GamePlayState(GAMEPLAYSTATE));
-//		this.addState(new ClientResourceLoader(LOADERSTATE, gDB));
+		// this.addState(new GamePlayState(GAMEPLAYSTATE));
+		// this.addState(new ClientResourceLoader(LOADERSTATE, gDB));
 		this.addState(new ClientGameplayState(CLIENTPLAYSTATE, entFac, player));
 		this.addState(new ClientLoaderState(CLIENTLOADSTATE, gDB));
 		this.enterState(CLIENTLOADSTATE);
 	}
 
-	//methods
+	// methods
 	@Override
 	public void initStatesList(GameContainer arg0) throws SlickException {
-//		this.getState(GAMEPLAYSTATE).init(arg0, this);
+		// this.getState(GAMEPLAYSTATE).init(arg0, this);
 	}
 
-    public static void main(String[] args) throws SlickException
-    {
-         AppGameContainer app = new AppGameContainer(new CoreStateManager());
-         app.setMouseGrabbed(false);
-         app.setDisplayMode(1024, 768, false);
-         app.start();
-    }
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new CoreStateManager());
+		app.setMouseGrabbed(false);
+		app.setDisplayMode(1024, 768, false);
+		app.start();
+//		BasicConfigurator.configure();
+//		NDC.push("ter");
+//		logger.info("Game Started");
+	}
 }
