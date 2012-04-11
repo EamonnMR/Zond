491ac890b41172790d561f5f98bd4bc143ba8132
Peter <pwroohr@gmail.com>
2012-04-11 18:35:08 -0400

    design doc.

--- RedShift/src/com/foo/redshift/core/GamePlayState.java
+++ RedShift/src/com/foo/redshift/core/GamePlayState.java
@@ -14,6 +14,7 @@ import com.foo.redshift.shifts.BasicEngine;
 import com.foo.redshift.shifts.BasicGun;
 import com.foo.redshift.shifts.BasicShip;
 
+@Deprecated
 public class GamePlayState extends BasicGameState {
 
     // vars
