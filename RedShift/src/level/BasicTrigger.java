931a11c518a9dc19e0098491be6343d2a5880cae
proohr <proohr@wl-proohr1.Virtusa.com>
2012-05-23 16:55:32 -0400

    - BasicLevel: starting to hash out how the level architecture is going to work - status: it's really rough right now, only 3 classes         Level: data class to house all necessary info         Trigger: a trigger entity, allows interactivity         Action: a behavior to do once a trigger is activated

---
+++ RedShift/src/level/BasicTrigger.java
@@ -0,0 +1,104 @@
+package level;
+
+import org.newdawn.slick.geom.Shape;
+
+/**
+ * Defines a basic Trigger entity that is used by the 
+ * BasicLevel class. 
+ * @author proohr
+ * @version 1.0
+ */
+public class BasicTrigger {
+
+	//variables
+	private Shape collider;
+	private String triggerName;
+	private String triggerTarget;
+	private float triggerX;
+	private float triggerY;
+	private boolean isActive;
+	private String triggerAction;
+
+	//constructor
+	BasicTrigger(){}
+	
+	//methods
+	public void update(int delta, boolean status, boolean triggered){
+		isActive = status;	//only do stuff if still alive
+		if(isActive){
+			
+			if(triggered){	//only do if triggered
+				
+			}
+		}
+	}
+	
+	public Shape getCollider() {
+		return collider;
+	}
+
+
+	public void setCollider(Shape collider) {
+		this.collider = collider;
+	}
+
+
+	public String getTriggerName() {
+		return triggerName;
+	}
+
+
+	public void setTriggerName(String triggerName) {
+		this.triggerName = triggerName;
+	}
+
+
+	public String getTriggerTarget() {
+		return triggerTarget;
+	}
+
+
+	public void setTriggerTarget(String triggerTarget) {
+		this.triggerTarget = triggerTarget;
+	}
+
+
+	public float getTriggerX() {
+		return triggerX;
+	}
+
+
+	public void setTriggerX(float triggerX) {
+		this.triggerX = triggerX;
+	}
+
+
+	public float getTriggerY() {
+		return triggerY;
+	}
+
+
+	public void setTriggerY(float triggerY) {
+		this.triggerY = triggerY;
+	}
+	
+	public boolean isActive() {
+		return isActive;
+	}
+
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+
+	public String getTriggerAction() {
+		return triggerAction;
+	}
+
+
+	public void setTriggerAction(String triggerAction) {
+		this.triggerAction = triggerAction;
+	}
+	
+}
