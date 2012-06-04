8e106f86df9b22d768f8b6caca7d1869f38cf46c
Roohr <pwroohr@gmail.com>
2012-06-04 00:21:35 -0400

    ini commit on SpawnShipAction.java

---
+++ RedShift/src/level/actions/SpawnShipAction.java
@@ -0,0 +1,47 @@
+package level.actions;
+
+import level.BasicAction;
+import core.ClientGameplayState;
+import ents.BasicShip;
+import ents.EntityFactory;
+
+
+/**
+ * Spawns a given ship and adds it to the ship hashmap
+ * @author Roohr
+ * @version 1.0
+ */
+public class SpawnShipAction extends BasicAction{
+
+	private float x,y;
+	private BasicShip ship;
+	private EntityFactory entFac;
+	private String sname,wep,eng;
+	
+	public SpawnShipAction(String name, float x, float y, String ship, String wep, String eng){
+		this.setName(name);
+		this.x = x;
+		this.y = y;
+		this.setIni(true);
+		this.setUpdate(false);
+		this.setDone(false);
+		this.sname = ship;
+	}
+	
+	@Override
+	public void ini(ClientGameplayState cgs){
+		setIni(false);
+		setUpdate(true);
+		this.entFac = cgs.getEntFac();
+		ship = entFac.buildShip(sname, eng,wep);
+	}
+	
+	@Override
+	public void update(int delta, ClientGameplayState cgs){
+		
+		ship.ini(x, y, 0.0f);
+		cgs.addShip(ship);
+		setUpdate(false);
+		setDone(true);
+	}
+}
