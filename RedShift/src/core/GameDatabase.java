e372321e56f96c35afda15f59169410b756977bc
Eamonn <Eamonn@Coke>
2012-06-05 23:45:02 -0400

    Minor cleanup of game database-merged the "loadImages" and "populateImages" methods.

--- RedShift/src/core/GameDatabase.java
+++ RedShift/src/core/GameDatabase.java
@@ -25,7 +25,6 @@ public class GameDatabase {
 
 	//vars
 //	private static GameDatabase instance;
-	private Image merc, gem, lunar, vost, vosk, zond4, engine1, gun1, level1, shot1, shot2,shot3, ast1;
 	private Map<String, Image> indexImages;
 	private Map<String, BasicShip> indexShip;
 	private Map<String, BasicGun> indexGuns;
@@ -45,12 +44,12 @@ public class GameDatabase {
 	 * indexShip() - always LAST
 	 */
 	public void iniGDB(){
+		indexImages  = new HashMap<String, Image>();
 		try {
 			loadImages();
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
-		indexImages  = new HashMap<String, Image>();
 		indexShot = new HashMap<String, BasicShot>();
 		indexGuns = new HashMap<String, BasicGun>();
 		indexEng = new HashMap<String, BasicEngine>();
@@ -64,7 +63,6 @@ public class GameDatabase {
 	 * 
 	 */
 	public void populateAll(){
-		populateImages();
 		populateArmor();
 		populateShot();
 		populateGun();
@@ -74,69 +72,46 @@ public class GameDatabase {
 	
 	/**
 	 * loadImages() - loads all images
+	 * Automate this before the beta
 	 * @throws SlickException
 	 */
 	public void loadImages() throws SlickException{
 		//Ships
-		merc = new Image("assets/images/ships/nasa/mercury.png");
-		gem = new Image("assets/images/ships/nasa/gemini.png");
-		lunar = new Image("assets/images/ships/nasa/lunar.png");
+		ldImg("merc","assets/images/ships/nasa/mercury.png");
+		ldImg("gem", "assets/images/ships/nasa/gemini.png");
+		ldImg("lunar","assets/images/ships/nasa/lunar.png");
 		
-		vost = new Image("assets/images/ships/russia/vostok1.png");
-		vosk = new Image("assets/images/ships/russia/voskhod.png");
-		zond4 = new Image("assets/images/ships/russia/zond4.png");
+		ldImg("vostok","assets/images/ships/russia/vostok1.png");
+		ldImg("voskhod","assets/images/ships/russia/voskhod.png");
+		ldImg("zond4","assets/images/ships/russia/zond4.png");
 		
 		//Engines
-		engine1 = new Image("assets/images/engines/engine1.png");
+		ldImg("engine1","assets/images/engines/engine1.png");
 		
 		//Guns
-		gun1 = new Image("assets/images/weapons/20mm.png");
+		ldImg("gun1","assets/images/weapons/20mm.png");
 		
 		//thrust fx
 //		thrust1 = new Image("assets/images/fx/engine1thrust.png");
 		
 		//Levels
-		level1 = new Image("assets/images/ScratchLevel.png");
+		ldImg("level1field","assets/images/ScratchLevel.png");
 		
 		//Shots
-		shot1 = new Image("assets/images/fx/shot1.png");
-		shot2 = new Image("assets/images/fx/shot2.png");
-		shot3 = new Image("assets/images/fx/laz1.png");
+		ldImg("shot1","assets/images/fx/shot1.png");
+		ldImg("shot2","assets/images/fx/shot2.png");
+		ldImg("shot3","assets/images/fx/laz1.png");
 		
 		//Asteroids
-		ast1 = new Image("assets/images/doodads/ast1.png");
+		ldImg("asteroid","assets/images/doodads/ast1.png");
 	}
-	
 	/**
-	 * puts Images into the index hashmap
-	 * procedural now, but automate before beta
+	 * Load an image into the database
+	 * @param name       It's name in the DB
+	 * @param location   Where to load the file from 
 	 */
-	public void populateImages(){
-		//Ships
-		indexImages.put("mercury", merc);
-		indexImages.put("gemini", gem);
-		indexImages.put("lunar", lunar);
-		
-		indexImages.put("vostok", vost);
-		indexImages.put("voskhod", vosk);
-		indexImages.put("zond4", zond4);
-		
-		//Engines
-		indexImages.put("eng1", engine1);
-		
-		//Guns
-		indexImages.put("gun1", gun1);
-		
-		//Levels
-		indexImages.put("level1field",level1);
-		
-		//Shots
-		indexImages.put("shot1",shot1);
-		indexImages.put("shot2", shot2);
-		indexImages.put("shot3", shot3);
-		
-		//Asteroids
-		indexImages.put("asteroid", ast1);
+	public void ldImg(String name, String location) throws SlickException{
+		indexImages.put(name, new Image(location) );
 	}
 	
 	/**
