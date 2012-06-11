95edda2826e0d5e54bb1d58a79e3182c3a1edc36
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2012-06-10 21:26:53 -0400

    * Made soem changes to StringTree over the weekend-it should not be capable of loading files.

--- RedShift/src/core/StringTree.java
+++ RedShift/src/core/StringTree.java
@@ -42,6 +42,56 @@ public class StringTree {
 		getNode(name).addChild(branch(name));
 	}
 	
+	/**
+	 * Parse a string tree from a string stream.
+	 * /
+	public static StringTree fromStream(InputStream inp){
+		toSender = StringTree();
+		toSender.root = parseBranch(inp, "root");
+	}
+
+	
+	//Get a string from an inputStream ending in a specific charicter
+	private static String getUntil(InputStream inp, char ender){
+		toSender = new String   //Algol had the idea of declaring return vars at the beginning of a function...
+		while( inp.hasNext() ){ //I think they had the right idea.
+			currentChar = (char)inp.next()
+			if (currentChar == ender){
+				return toSender;
+			} else {
+				toSender.ADDCHAR( currentChar );
+			}
+		}
+		//If it does not reach char 'ender' it probably fowls the entire input read,
+		//so print a warning.
+		System.out.println( "BAD INPUT: MISSING ".ADDCHAR( ender ) );
+		return toSender;
+	}
+
+	private static StringTree.Node parseBranch(InputStream inp, String name){
+		currentNode = branch(name);
+		nextName = String(); //Name of the next node
+		while( inp.hasNext() ){
+			switch( (char) inp.next() ){
+				case ( '[' ){ //It's a label applying to the next node.
+					nextName = getUntil(inp, ']');
+				}
+				case ( '(' ){ //It's a leaf node, load the text and attach it to the branch
+					currentNode.addChild( leaf( nextName, getUntil( inp, ')' ) ) );
+				}
+				case ( '{' ){ //This is the recursive bit-if it's another branch, recurse!
+					currentNode.addChild( parseBranch( inp, nextName) )
+				}
+				case ( '}' ){ //End of branch
+					return currentNode
+				}
+			}
+		} //This should never be passed except if it's the root node, which is in fact terminated by
+		if (name != root){ //The end of the file.
+			System.out.println( "BAD INPUT: MISSING } ");
+			 
+		
+
 	private StringTree.Node branch(String name){
 		Node toSender = new Node(name);
 		toSender.childNodes = new HashMap<String, Node>();
@@ -54,6 +104,18 @@ public class StringTree {
 		return toSender;
 	}
 	
+	/**
+	 * Get the names of the child nodes of a given Branch.
+	 */
+
+	public ____ listChildren(String... name){
+		Node parent = root;
+		for (String n : name){
+			parent = parent.getChild(n);
+		}
+		return parent.children.keys();
+	}
+
 	private Node getNode(String... name){
 		Node toSender = root;
 		for (String n : name){
