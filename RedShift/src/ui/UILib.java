8b77d0689080132830ddc0056d7337007309d8e2
Roohr <pwroohr@gmail.com>
2012-10-07 02:16:55 -0400

    -update: phase shifting UI from text junk too images per EMR's suggestions -added: a slew of ui images, and more coming

--- RedShift/src/ui/UILib.java
+++ RedShift/src/ui/UILib.java
@@ -1,6 +1,8 @@
 package ui;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 /**
@@ -23,9 +25,11 @@ public class UILib {
 	 * @param gfx graphics
 	 */
 	public void drawStringAtShapeCenter(String s, Shape b, Graphics gfx){
-		float diff = getStringPixelWidth(s);
-		float str_x = b.getCenterX()-(diff/2);
-		gfx.drawString(s, str_x, b.getCenterY()-8);
+		float width = getStringPixelWidth(s);
+		float height = getStringPixelHeight(s); 
+		float str_x = b.getCenterX()-(width/2);
+		float str_y = b.getCenterY()-height;
+		gfx.drawString(s, str_x, str_y);
 	}
 	
 	/**
@@ -59,8 +63,75 @@ public class UILib {
 	 * @return width in pixels
 	 */
 	public int getStringPixelWidth(String s){
-		return s.length()*10;
+		int nl = 0;
+		for(int i=0; i<s.length();i++){
+			if(s.charAt(i)=='\n'){
+				nl = i;
+				break;
+			}
+		}
+		if(nl!=0){
+			String str = s.substring(0, nl);
+			return str.length()*10;
+		}else{
+			return s.length()*10;
+		}
 	}
 	
+	/**
+	 * simply gets the height in pixels of a given string
+	 * @param s
+	 * @return
+	 */
+	public int getStringPixelHeight(String s){
+		int nl=0;
+		for(int i=0; i<s.length();i++){
+			if(s.charAt(i)=='\n'){
+				nl++;
+			}
+		}
+		if(nl>=1){
+			return nl*14;
+		}else{
+			return 8;
+		}
+	}
+	
+	/**
+	 * draws a rectangle around a given string;
+	 * @param s
+	 * @param x
+	 * @param y
+	 * @param gfx
+	 */
+	public void drawRectangleAroundString(String s, float x, float y, Graphics gfx){
+		Rectangle rec;
+		int len = getStringPixelWidth(s);
+		int h = getStringPixelHeight(s);
+		rec = new Rectangle(x-2, y-2, len+4,h+14);
+		gfx.draw(rec);
+	}
+	
+	
+	/**
+	 * draws a shape with a given color
+	 * @param b
+	 * @param color
+	 */
+	public void highlightShape(Shape b, Color color, Graphics gfx){
+		gfx.setColor(color);
+		gfx.draw(b);
+	}
+	
+	/**
+	 * draws a string with a given color
+	 * @param s
+	 * @param color
+	 * @param gfx
+	 */
+	public void highlightString(String s, Color color, Graphics gfx, float x, float y){
+		gfx.setColor(color);
+		gfx.drawString(s, x, y);
+	}
 	
 }
