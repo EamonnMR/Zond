78409d4b147bd27a0f6765c41b8694d339477562
proohr <proohr@wl-proohr1.Virtusa.com>
2012-12-07 10:21:27 -0500

    -updates:         -scenario 1 added death trigs to ship desc         -game database knows about death trigs         -same with entfac and shipdesc -removed:         -level builder class, its time has passed         -this affected core, gameplay, loaderstate

--- RedShift/src/ents/ShipDesc.java
+++ RedShift/src/ents/ShipDesc.java
@@ -1,18 +1,19 @@
 package ents;
 
 public class ShipDesc {
-	public String kind, gun, engine;
+	public String kind, gun, engine, trigtarg;
 	public double x, y;
 	public effects.Effect fx;
 	public boolean isAi;
 	
-	public ShipDesc(String kind, String gun, String engine, double[] pt, boolean isAi) {
+	public ShipDesc(String kind, String gun, String engine, double[] pt, boolean isAi, String trig) {
 		this.kind = kind;
 		this.gun = gun;
 		this.engine = engine;
 		this.x = pt[0];
 		this.y = pt[1];
 		this.isAi = isAi;
+		this.trigtarg = trig;
 	}
 
 	@Override
